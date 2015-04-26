777e583f19e9ae0888b8c31cc391d7b4117f2315
matthias <matthias@hobbes>
2015-04-26 22:24:15 +0200

    until mounting of file systems

--- arch-install.sh
+++ arch-install.sh
@@ -26,4 +26,96 @@ fi
 
 . "$INSTALL_CONF"
 
-printf "$INSTALL_DEVICE\n"
+doDeactivateAllSwaps() {
+	swapoff -a
+}
+
+getAllPartitions() {
+	lsblk -l -n -o NAME "$INSTALL_DEVICE" | grep -v "^`basename "$INSTALL_DEVICE"`$"
+}
+
+doWipeAllPartitions() {
+	for i in $( getAllPartitions | sort -r ); do
+		dd if=/dev/zero of=/dev/"$i" bs=10240k count=1
+	done
+	sync
+}
+
+doDeleteAllPartitions() {
+	fdisk "$INSTALL_DEVICE" << __END__
+o
+w
+__END__
+	sync
+}
+
+doWipeDevice() {
+	dd if=/dev/zero of="$INSTALL_DEVICE" bs=10240k count=1
+	sync
+}
+
+doCreateNewPartitions() {
+	parted -a optimal "$INSTALL_DEVICE" << __END__
+mklabel msdos
+u MiB
+mkpart primary linux-swap 1 $BOOT_SIZE
+mkpart primary linux-swap $BOOT_SIZE 100%
+toggle 1 boot
+quit
+__END__
+	sync
+
+	fdisk "$INSTALL_DEVICE" << __END__
+t
+1
+83
+t
+2
+8e
+w
+__END__
+	sync
+}
+
+identifyPartitions() {
+	ALL_PARTITIONS=($( getAllPartitions ))
+	BOOT_PARTITION="/dev/${ALL_PARTITIONS[0]}"
+	LUKS_PARTITION="/dev/${ALL_PARTITIONS[1]}"
+}
+
+doCreateLuksLvm() {
+	cryptsetup -c aes-xts-plain64 -s 512 -h sha512 luksFormat "$LUKS_PARTITION"
+	cryptsetup luksOpen "$LUKS_PARTITION" lvm
+}
+
+doCreateLvmVolumes() {
+	pvcreate /dev/mapper/lvm
+	vgcreate main /dev/mapper/lvm
+	lvcreate -L "$SWAP_SIZE"M -n swap main
+	lvcreate -l 100%FREE -n root main
+}
+
+doFormat() {
+	mkfs.ext2 -L boot "$BOOT_PARTITION"
+	mkswap -L swap /dev/mapper/main-swap
+	mkfs.ext4 -L root /dev/mapper/main-root
+}
+
+doMount() {
+	mount /dev/mapper/main-root /mnt
+	mkdir /mnt/boot
+	mount "$BOOT_PARTITION" /mnt/boot
+	swapon /dev/mapper/main-swap
+}
+
+doDeactivateAllSwaps
+doWipeAllPartitions
+doDeleteAllPartitions
+doWipeDevice
+doCreateNewPartitions
+doWipeAllPartitions
+identifyPartitions
+doCreateLuksLvm
+doCreateLvmVolumes
+doFormat
+doMount
