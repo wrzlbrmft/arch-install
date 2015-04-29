b21146216e52cf4150a819e606e60d773a3673c9
matthias <matthias@hobbes>
2015-04-29 19:32:44 +0200

    refactoring

---
+++ arch-install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+INSTALL_HOME=$( cd "`dirname "${BASH_SOURCE[0]}"`" && pwd )
+INSTALL_SCRIPT="`basename "${BASH_SOURCE[0]}"`"
+INSTALL_NAME="`printf "$INSTALL_SCRIPT" | awk -F '.' '{ print $1 }'`"
+
+doPrintHelpMessage() {
+	printf "Usage: ./$INSTALL_SCRIPT [-h] [-c <config>] [<action>]\n"
+}
+
+while getopts :hc: opt; do
+	case "$opt" in
+		h)
+			doPrintHelpMessage
+			exit 0
+			;;
+
+		c)
+			INSTALL_CONFIG="$OPTARG"
+			;;
+
+		:)
+			printf "ERROR: "
+			case "$OPTARG" in
+				c)
+					printf "Missing config file"
+					;;
+			esac
+			printf "\n"
+			exit 1
+			;;
+
+		\?)
+			printf "ERROR: Invalid option ('-$OPTARG')\n"
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND - 1))
+
+INSTALL_ACTION="$1"
+
+if [ -z "$INSTALL_CONFIG" ]; then
+	INSTALL_CONFIG="$INSTALL_HOME/$INSTALL_NAME.conf"
+fi
+
+if [ ! -f "$INSTALL_CONFIG" ]; then
+	printf "ERROR: Config file not found ('$INSTALL_CONFIG')\n"
+	exit 1
+fi
+
+if [ -z "$INSTALL_ACTION" ]; then
+	INSTALL_ACTION="base"
+fi
+
+. "$INSTALL_CONFIG"
